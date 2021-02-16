16561f9ffa8b45c0297c8956f11f8b6ba5358f39
Jordan <jordancaley10@gmail.com>
2021-02-16 18:55:59 +0100

    Add team and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/contact', to: 'contact#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/team', to: 'team#index'
 end
