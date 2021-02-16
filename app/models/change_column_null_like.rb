b790f1322679af926a4248e8532487b8db760dc9
Jordan <jordancaley10@gmail.com>
2021-02-16 15:20:13 +0100

    Import migrations, models and seed from previous version

---
+++ app/models/change_column_null_like.rb
@@ -0,0 +1,2 @@
+class ChangeColumnNullLike < ApplicationRecord
+end
---
+++ db/migrate/20210216135737_create_change_column_null_likes.rb
@@ -0,0 +1,6 @@
+class CreateChangeColumnNullLikes < ActiveRecord::Migration[6.1]
+  def change
+    change_column_null :likes, :gossip_id, true
+    change_column_null :likes, :comment_id, true
+  end
+end
