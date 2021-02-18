fbac1955fda2cbbda07301d673027f3f211abb13
Jordan <jordancaley10@gmail.com>
2021-02-18 15:07:23 +0100

    Change default welcome name to User

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   get 'gossip/:id', to: 'gossip#show', as: 'gossip'
   get 'welcome/:first_name', to: 'welcome#show', as: 'welcome'
   get '/contact', to: 'contact#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/team', to: 'team#index'
 end
