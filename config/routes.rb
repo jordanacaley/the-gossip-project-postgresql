adb23f458f85e941246d482e52a3574119cadc1f
Jordan <jordancaley10@gmail.com>
2021-02-18 14:23:26 +0100

    Create link to user profile page on each gossip page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'user/:id', to: 'user#show', as: 'user'
   get 'gossip/:id', to: 'gossip#show', as: 'gossip'
   get 'welcome/:first_name', to: 'welcome#show', as: 'welcome'
   get '/contact', to: 'contact#index'
