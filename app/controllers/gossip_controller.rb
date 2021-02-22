ce6b1a8ed20b4bd66967b24d738adf3195564a06
Jordan <jordancaley10@gmail.com>
2021-02-22 10:00:27 +0100

    Update routes using resources, add methods to gossip controller, create view for new gossip

--- app/controllers/gossip_controller.rb
+++ app/controllers/gossip_controller.rb
@@ -1,8 +1,34 @@
 class GossipController < ApplicationController
+  
+  def index
+
+  end
+  
   def show
     puts "$" * 60
     puts "Voici le message de l'URL :"
     puts params[:id]
     puts "$" * 60
   end
+
+  def new
+
+  end
+
+  def create
+    
+  end
+
+  def edit
+
+  end
+
+  def update
+    
+  end
+
+  def destroy
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'user/:id', to: 'user#show', as: 'user'
-  get 'gossip/:id', to: 'gossip#show', as: 'gossip'
-  get 'welcome/:first_name', to: 'welcome#show', as: 'welcome'
-  get '/contact', to: 'contact#index'
-  get '/team', to: 'team#index'
+  resources :gossip
 end
