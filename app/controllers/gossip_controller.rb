9223ed1d48a068fb7205f7fcd51717309361e437
Jordan <jordancaley10@gmail.com>
2021-02-22 14:36:51 +0100

    Add create method for gossip

--- app/controllers/gossip_controller.rb
+++ app/controllers/gossip_controller.rb
@@ -1,42 +1,53 @@
 class GossipController < ApplicationController
   
   def index
-    render 'index'
+    @gossips = Gossip.all
   end
   
   def show
-    puts "$" * 60
-    puts "Voici le message de l'URL :"
-    puts params[:id]
-    puts "$" * 60
+    @gossip = Gossip.find(params[:id])
   end
 
   def new
-
+    @gossip = Gossip.new
   end
 
   def create
-    @gossip = Gossip.new('title' => params[:title],
-                          'content' => params[:content],
-                          'user_id' => 12)
-
+    @gossip = Gossip.new(gossip_params)
+    puts @gossip.user_id
+    puts @gossip.title
+    puts @gossip.content
     if @gossip.save 
-      redirect_to gossip_path
+      redirect_to gossip_index_path
     else
       render 'new'
     end    
   end
 
   def edit
-
+    @gossip = Gossip.find(params[:id])
   end
 
   def update
-    
+    @gossip = Gossip.find(params[:id])
+
+    if @gossip.update(params[:gossip].permit(:title, :content))
+      redirect_to gossip_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
 
+    redirect_to gossip_path
   end
+
+  private
+    def gossip_params
+      params.require(:gossip).permit(:title, :content, :user_id)
+    end
 
 end
