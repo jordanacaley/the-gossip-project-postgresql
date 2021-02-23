4579c33ff9149cd69d0dbecbc7a5cd10af922abd
Jordan <jordancaley10@gmail.com>
2021-02-23 11:31:10 +0100

    Add show city page accessible from user show and gossip show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'city/show'
   resources :gossip
   resources :user
+  resources :city
   get '/contact', to: 'contact#index'
   get '/team', to: 'team#index'
 end
